fb337bfd9ad24c64049e08697b5e9de8d99f087e
Colin Peppler <colinpeppler@gmail.com>
2020-11-21 15:19:51 -0500

    Developing base for impulsive buy card

--- buyNowClickHandler.js
+++ buyNowClickHandler.js
@@ -1,14 +1,20 @@
 'use strict';
 
+
+/* Create dialog modal */
+const dialog = document.createElement('dialog')
+dialog.id = 'our-dialog';
+dialog.innerHTML = `
+	<div style="height:460px; width:390px; box-shadow:0px 1px 3px #0000002C; border-radius: 5px;">
+		I am a dialog! 
+	</div>
+`;
+
 const page = document.getElementById('a-page')
+page.appendChild(dialog);
+
 const buyNowButton = document.getElementById('buy-now-button')
-
 if (buyNowButton != null) {
-	const dialog = document.createElement('dialog')
-	dialog.id = 'our-dialog';
-	dialog.innerHTML = '<div style="height:400px;width:300px"> I am a dialog! </div>';
-	page.appendChild(dialog);
-
 	// cancels the onClick
 	$("#buy-now-button").click(ev => { ev.preventDefault(); });
 	buyNowButton.addEventListener('click', () => {
