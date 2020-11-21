06af6a6f00f8baca66744509c630c20189bb1bd1
Colin Peppler <colinpeppler@gmail.com>
2020-11-21 11:28:40 -0500

    rm annoying sleep err

--- buyNowClickHandler.js
+++ buyNowClickHandler.js
@@ -2,8 +2,5 @@ const buyNowButton = document.getElementById('buy-now-button')
 if (buyNowButton != null) {
 	buyNowButton.onclick = () => {
 		console.log('yeet.. i sleep now 5 secs')
-		sleep(5);
 	};
-}
 
-const sleep = secs => await new Promise(r => setTimeout(r, 1000 * secs));
