074c4ad1aa5a918967bd5a39617fd02700fe6112
Colin Peppler <colinpeppler@gmail.com>
2020-11-21 15:12:57 -0500

    Disable onClick for Buy Now, show modal

--- buyNowClickHandler.js
+++ buyNowClickHandler.js
@@ -1,14 +1,17 @@
+'use strict';
+
 const page = document.getElementById('a-page')
 const buyNowButton = document.getElementById('buy-now-button')
+
 if (buyNowButton != null) {
 	const dialog = document.createElement('dialog')
 	dialog.id = 'our-dialog';
-	dialog.innerHTML = 'I am a dialog!';
+	dialog.innerHTML = '<div style="height:400px;width:300px"> I am a dialog! </div>';
 	page.appendChild(dialog);
 
+	// cancels the onClick
+	$("#buy-now-button").click(ev => { ev.preventDefault(); });
 	buyNowButton.addEventListener('click', () => {
-		//dialog.show()		//it won't show :(
-		alert('yeet')
+		dialog.showModal()
 	});
 }
-
