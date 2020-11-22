78db5b3466f03de0abee75dbf16a091dc6218657
zifa25796 <59847842+zifa25796@users.noreply.github.com>
2020-11-21 22:48:25 -0500

    New pet system

--- pets.js
+++ pets.js
@@ -1,40 +1,41 @@
 function Pet(){
     this.level = 0
-    this.points = 0
-    this.required_points = 25 * (this.level + 1)
-    this.branch = undefined
-    this.img = "res/init.png"
-    this.types = ["Cat", "Dog", "Rabbit", "Panda"]
+    this.exp = 0
+    this.branch = "Default"
+}
 
-    function rand_branch(){
+function rand_branch(){
+    chrome.storage.local.get(["My_Pet"], function(ret){
+        pet_struct = ret.My_Pet
+
+        types = ["Cat", "Dog", "Rabbit", "Panda"]
         var date = new Date()
-        rand_seed = Math.floor((date.getMilliseconds / 100)/2.5)
-        this.branch = rand_seed
-        this.change_ing()
-    }
+        rand_seed = Math.floor((date.getMilliseconds() / 100)/2.5)
+        pet_struct.branch = types[rand_seed]
+        chrome.storage.local.set({My_Pet: pet_struct}, function() {
+        })
+        console.log(ret)
+    })
+    
+}
 
-    function change_ing(){
-        img_str = ""
-        this.img = img_str.concat("res/", this.types[this.branch], ".png")
-    }
 
-    function add_points(p){
-        if (this.level == "MAX"){
+function add_points(p){
+    chrome.storage.local.get(["My_Pet"], function(ret){
+        pet_struct = ret.My_Pet
+        if (pet_struct.level == 2){
             return
         }
-        this.points += p
-        if (this.points >= this.required_points) {
-            this.points -= this.required_points
-            this.level ++
-            this.change_ing()
-            if (this.level == 2){
-                this.level = "MAX"
-                return
-            }
-            else if (this.level == 1){
-                this.rand_branch()
-            }
-            this.required_points = 25 * (this.level + 1)
+        level_arr = [50, 100, 150]
+        pet_struct.exp += p
+        if (pet_struct.exp >= level_arr[pet_struct.level]){
+            pet_struct.level++
         }
-    }
+        chrome.storage.local.set({My_Pet: pet_struct}, function() {
+            if (pet_struct.level == 1){
+                rand_branch()
+            }
+        })
+    })
+
 }
