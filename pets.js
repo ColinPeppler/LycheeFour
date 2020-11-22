730372f0992ede79aa4d04828a5c53599eb6ef8d
zifa25796 <59847842+zifa25796@users.noreply.github.com>
2020-11-22 00:08:32 -0500

    Changed to Rand @ level 0

--- pets.js
+++ pets.js
@@ -2,6 +2,8 @@ function Pet(){
     this.level = 0
     this.exp = 0
     this.branch = "Default"
+
+    rand_branch()
 }
 
 function rand_branch(){
@@ -19,6 +21,16 @@ function rand_branch(){
     
 }
 
+function get_petName(){
+    chrome.storage.local.get(["My_Pet"], function(ret){
+        pet_struct = ret.My_Pet
+        var str = ""
+        str = pet_struct.branch + Number(pet_struct.level)
+        
+        console.log(str)
+    })
+}
+
 
 function add_points(p){
     chrome.storage.local.get(["My_Pet"], function(ret){
@@ -33,20 +45,7 @@ function add_points(p){
         }
         console.log(pet_struct)
         chrome.storage.local.set({My_Pet: pet_struct}, function() {
-            if (pet_struct.level == 1){
-                rand_branch()
-            }
         })
     })
 
-}
-
-function get_petName(){
-    chrome.storage.local.get(["My_Pet"], function(ret){
-        pet_struct = ret.My_Pet
-        var str = ""
-        str = pet_struct.branch + Number(pet_struct.level)
-        
-        console.log(str)
-    })
 }
